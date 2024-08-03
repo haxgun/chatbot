12ba6d557ac5df119534b6e86c18840ce71b6192
Daniil <157521979+GoToProd@users.noreply.github.com>
2024-08-03 20:58:10 +0300

    Update test.py
    test new function.

--- test.py
+++ test.py
@@ -1,44 +1,62 @@
-import httpx
 import openai
+import telebot
+import logging
 import os
+import time
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
-proxy_url = os.getenv("PROXY_OPEN_AI")
 apikey = os.getenv("OPENAI_API_KEY")
+bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
 
-
-# # Настройка клиента httpx с прокси, если прокси указан
-if proxy_url:
-    client = httpx.Client(
-        proxies={
-            "http://": str(proxy_url),
-            "https://": str(proxy_url),
-        }
-    )
-else:
-    client = httpx.Client()
-
-# Настройка клиента OpenAI с использованием httpx клиента
-openai.api_key = apikey
-openai.aiosession.set(client)
-
-# Запрос на создание чата
-completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {
-            "role": "system",
-            "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
-        },
-        {
-            "role": "user",
-            "content": "Compose a poem that explains the concept of recursion in programming.",
-        },
-    ],
+log_dir = os.path.join(os.path.dirname(__file__), "ChatGPT_Logs")
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+logging.basicConfig(
+    filename=os.path.join(log_dir, "error.log"),
+    level=logging.ERROR,
+    format="%(levelname)s: %(asctime)s %(message)s",
+    datefmt="%d/%m/%Y %H:%M:%S",
 )
 
-# Вывод результата
-print(completion.choices[0].message["content"])
+
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    bot.reply_to(
+        message,
+        "Привет!\nЯ ChatGPT 3.5 Telegram Bot\U0001F916\nЗадай мне любой вопрос и я постараюсь на него ответиь",
+    )
+
+
+def generate_response(prompt):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}]
+    )
+    return completion.choices[0].message.content
+
+
+@bot.message_handler(commands=["bot"])
+def command_message(message):
+    prompt = message.text
+    response = generate_response(prompt)
+    bot.reply_to(message, text=response)
+
+
+@bot.message_handler(func=lambda _: True)
+def handle_message(message):
+    prompt = message.text
+    response = generate_response(prompt)
+    bot.send_message(chat_id=message.from_user.id, text=response)
+
+
+print("ChatGPT Bot is working")
+
+while True:
+    try:
+        bot.polling()
+    except (telebot.apihelper.ApiException, ConnectionError) as e:
+        logging.error(str(e))
+        time.sleep(5)
+        continue
